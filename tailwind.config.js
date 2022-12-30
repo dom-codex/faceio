04bd1158fd6201a71f4049be59cb0596641546d5
dominic <dominicwest7@gmail.com>
2022-12-30 04:02:47 -0800

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content:["./src/**/*.{js,jsx}"],
+    theme:{
+        extend:{
+            fontFamily:{
+                "daniel":["daniel"],
+                "boo":["boo"],
+                "bsans":["bsans"]
+            },
+            colors:{
+                "skyblue":"#00b5E2",
+                "silver":"#BEBBBA",
+                "sandyBrown":"#EEAF52",
+                "mediumGreen":"63BA64"
+            }
+        }
+    }
+}
